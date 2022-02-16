8bb4669a8b9db5f46a3eefb5b80ada35dc2bf033
Lucas Fernandes <lucas_rfernandes@hotmail.com>
2022-02-16 14:08:55 -0300

    Aula 02-05

--- aula-02-atividade-03-splice-array.js
+++ aula-02-atividade-03-splice-array.js
@@ -1,8 +1,16 @@
-let alunos = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo']
-//Remover 2 elementos (Ana e Caio) e adicionar um novo elemento (Rodrigo)
-alunos.splice(1,2, 'Rodrigo') 
-// 1 = indice inicial, 2  = quantidade de elementos removidos na posição inicial, Rodrigo = elemento adicionado na posição inicial
 
-//Apenas adicionar um novo elemento (Fernanda) após o Rodrigo
+//Indices =     0       1       2      3       4          5
+let alunos = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo']
+//Exercicio: Remover 2 elementos (Ana e Caio) e adicionar 3 novos elementos (Rodrigo, Leandro e Fracine)
+alunos.splice(1,2, 'Rodrigo', 'Leandro', 'Francine') 
+//1 = define o indice inicial = Ana; 
+//2 = define a quantidade de elementos removidos a partir da posição inicial = Ana e Caio; 
+//Rodrigo, Leandro, Francine = representa os elemento(s) adicionado(s) a partir da posição inicial
+console.log(alunos)
+
+//Exercicio: adicionar um novo elemento (Fernanda) após o Rodrigo
 alunos.splice(2,0, 'Fernanda') 
+//2 = define o indice inicial = Leandro; 
+//0 = define a quantidade de elementos removidos a partir da posição inicial; 
+//Fernanda = representa os elemento(s) adicionado(s) a partir da posição inicial
 console.log(alunos)
